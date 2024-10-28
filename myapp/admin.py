501508dd3fd37507548cf5f47eac73b606465219
Jinwoo Ha <jinwooha79@gmail.com>
2024-10-28 15:22:41 +0900

    이미지 관련 코드 주석 처리 전

--- myapp/admin.py
+++ myapp/admin.py
@@ -6,6 +6,3 @@ class DocumentAdmin(admin.ModelAdmin):
     list_display = ['id', 'project_title', 'created_at', 'image_folder']  # 목록에서 보여줄 필드들
     search_fields = ['project_title', 'project_description']  # 검색 가능한 필드들
     list_filter = ['created_at']  # 필터링 가능한 필드들
-
-# 또는 더 간단하게:
-# admin.site.register(Document)
